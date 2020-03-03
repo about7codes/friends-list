e7439e7f948ade64161b2b2636ee857416294cbb
about7code <sanaullahhaifz@gmail.com>
2020-03-03 20:06:12 +0400

    Deploying React app on Github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => <PersonList />;
+
+const PersonList = () => {
+  const people = [
+    {
+      img: Math.floor(Math.random() * 90),
+      name: "Emma",
+      job: "Developer"
+    },
+    {
+      img: Math.floor(Math.random() * 90),
+      name: "Sophia",
+      job: "Designer"
+    },
+    {
+      img: Math.floor(Math.random() * 90),
+      name: "Isabella",
+      job: "Artist"
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section>
+      <Person person={people[0]}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam magnam
+        excepturi totam quas asperiores maiores eum fugiat quam, placeat
+        molestiae.
+      </Person>
+      <Person person={people[1]}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+        obcaecati impedit consequatur neque suscipit architecto, voluptatem ea
+        deleniti iusto error.
+      </Person>
+      <Person person={people[2]}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+        provident minima consequuntur explicabo ducimus officiis, rem
+        cupiditate? Esse, non officiis?
+      </Person>
+    </section>
+  );
+};
+
+const Person = props => {
+  const { img, name, job } = props.person;
+  const { children } = props;
+  const url = `https://randomuser.me/api/portraits/thumb/women/${img}.jpg`;
+  return (
+    <div className="person">
+      <img src={url} alt="" />
+      <div>
+        <h4>{name}</h4>
+        <h4>{job}</h4>
+        {children}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
