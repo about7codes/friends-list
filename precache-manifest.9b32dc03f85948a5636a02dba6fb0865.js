15bc3714d896c32d6f85532640d7d67132e42310
about7code <sanaullahhaifz@gmail.com>
2020-03-03 20:07:23 +0400

    Updates

---
+++ precache-manifest.9b32dc03f85948a5636a02dba6fb0865.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "663e080a04f87d33b0b2cf4f3c99e72f",
+    "url": "/about7codes/friends-list/index.html"
+  },
+  {
+    "revision": "06e52339c590f87059f3",
+    "url": "/about7codes/friends-list/static/css/main.325ee868.chunk.css"
+  },
+  {
+    "revision": "cf110d6bb7d49a30d46a",
+    "url": "/about7codes/friends-list/static/js/2.f92396c5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/about7codes/friends-list/static/js/2.f92396c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e52339c590f87059f3",
+    "url": "/about7codes/friends-list/static/js/main.dd7d238c.chunk.js"
+  },
+  {
+    "revision": "b3a929f96c3b98b9e1c7",
+    "url": "/about7codes/friends-list/static/js/runtime-main.887b3134.js"
+  }
+]);
